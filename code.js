c5e4f28fe27792e4b055aed4d3d6f9a283b84c94
Meyakakhu <81014267+Meyakakhu@users.noreply.github.com>
2022-06-29 20:32:43 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 
-        console.log("version: 0.0.21");
+        console.log("version: 0.0.22");
         var loaded = false;
         scan_users();
         setInterval(function () {
@@ -16,8 +16,6 @@
         }, 5000);
         var chat = null;
         function loadscript(){
-            var chat = document.getElementsByClassName("ReactVirtualized__Grid__innerScrollContainer")[1];
-            chat.style = "width: auto;  max-width: 362px;  overflow: hidden; pointer-events: none; position: relative;";
             var settings_menu = document.getElementsByClassName("right--Z4dKWq")[0];
             var simple_settings = document.createElement("div");
             simple_settings.id = "simple_settings_f23f23f";
@@ -31,6 +29,8 @@
             create_settings_panel();
             add_record_button();
                 
+            chat = document.getElementsByClassName("ReactVirtualized__Grid__innerScrollContainer")[1];
+            chat.style = "width: auto;  max-width: 362px;  overflow: hidden; pointer-events: none; position: relative;";
             generate_message("Скрипт загружен");
         }
 
